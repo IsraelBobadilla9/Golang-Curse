476b5fa559e1712b41d7f3ebbe65c9bb8592e48b
IsraelBobadilla9 <israelbobadilla9@hotmail.com>
2025-04-22 11:38:48 -0600

    - Retorno de valores simples y multiples

--- clase_2/test.go
+++ clase_2/test.go
@@ -9,7 +9,12 @@ func main() {
 
 	miFuncion()
 	miFUncionConParametros(5, 10)
+	valorRetorno := miFuncionConRetorno("Israel")
+	fmt.Println(valorRetorno)
 
+	nombre, apellido, edad := miFuncionConRetornoMultiple("israel", "bobadilla", 20)
+
+	fmt.Println(nombre, apellido, edad)
 }
 
 // Declaracion de funcion sin modularizar
@@ -22,4 +27,12 @@ func miFUncionConParametros(n1 int, n2 int) {
 	resultado := n1 + n2
 	fmt.Println("Parametro uno ", n1, "Segundo paramtro ", n2)
 	fmt.Println("La suma es ", resultado)
+}
+
+func miFuncionConRetorno(nombre string) string {
+	return "Retornando valor desde la funcion " + nombre
+}
+
+func miFuncionConRetornoMultiple(nombre string, apellido string, edad int) (string, string, int) {
+	return "Hola " + nombre, "Apellido: " + apellido, edad
 }
